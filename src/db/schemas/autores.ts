a872a5f654af717598672eabc8a4314f077d2938
BMNofc <mauricio2006k@outlook.com>
2026-04-02 22:46:59 -0300

    drizzle

--- src/db/schemas/autores.ts
+++ src/db/schemas/autores.ts
@@ -1,7 +1,7 @@
-import { int, mssqlTable, varchar, } from "drizzle-orm/mssql-core";
+import { int, mssqlTable, varchar } from 'drizzle-orm/mssql-core';
 
 export const autoresTable = mssqlTable('autores', {
-    id: int().primaryKey().identity(),
-    nome: varchar({ length: 100}).notNull(),
-    email: varchar({ length: 255}).notNull().unique(),
+  id: int().primaryKey().identity(),
+  nome: varchar({ length: 100 }).notNull(),
+  email: varchar({ length: 255 }).notNull().unique(),
 });
